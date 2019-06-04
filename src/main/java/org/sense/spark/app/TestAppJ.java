24debeafcf34ae89204487b78020b6237d0fb98d
Felipe Oliveira Gutierrez <felipe.o.gutierrez@gmail.com>
2019-06-04 16:32:40 +0200

    application to test Java package

---
+++ src/main/java/org/sense/spark/app/TestAppJ.java
@@ -0,0 +1,9 @@
+package org.sense.spark.app;
+
+import org.apache.commons.collections.bag.SynchronizedSortedBag;
+
+public class TestAppJ {
+    public static void main(String[] args) {
+        System.out.println("Application to test java package");
+    }
+}
