0b0c1c9c46ebd653eadb4861fdc1320bb58a2b44
Felipe Gutierrez <felipe.gutierrez@tu-berlin.de>
2019-10-11 14:34:58 +0200

    Add logLevels https://github.com/felipegutierrez/dynamic-combiner/issues/1

--- src/main/scala/org/sense/spark/app/TestStreamCombineByKey.scala
+++ src/main/scala/org/sense/spark/app/TestStreamCombineByKey.scala
@@ -3,49 +3,54 @@ package org.sense.spark.app
 import org.apache.spark.rdd.RDD
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 import org.apache.spark.{HashPartitioner, SparkConf}
+import org.sense.spark.util.StreamingExamples
+
+import scala.collection.mutable.Queue
 
 object TestStreamCombineByKey {
   def main(args: Array[String]): Unit = {
 
+    StreamingExamples.setStreamingLogLevels()
     // Create a local StreamingContext with two working thread and batch interval of 1 second.
-    // The master requires 2 cores to prevent from a starvation scenario.
-    val conf = new SparkConf().setMaster("local[2]").setAppName("LocalWordCount")
-    val ssc = new StreamingContext(conf, Seconds(5))
+    // The master requires 4 cores to prevent from a starvation scenario.
+    val sparkConf = new SparkConf()
+      .setAppName("QueueStreamWordCount")
+      .setMaster("local[4]")
+    val ssc = new StreamingContext(sparkConf, Seconds(5))
 
     // Create a DStream that will connect to hostname:port, like localhost:9999
+    val rddQueue = new Queue[RDD[String]]()
+    val lines = ssc.queueStream(rddQueue)
     // val lines = ssc.socketTextStream("localhost", 9999)
-    val queue = new scala.collection.mutable.Queue[RDD[String]]
+
+    val wordCounts = lines
+      .flatMap(_.split(" "))
+      .map(word => (word, 1))
+      // .reduceByKey(_ + _)
+      .combineByKey(
+        (v) => (v, 1), //createCombiner
+        (acc: (Int, Int), v) => (acc._1 + v, acc._2 + 1), //mergeValue
+        (acc1: (Int, Int), acc2: (Int, Int)) => (acc1._1 + acc2._1, acc1._2 + acc2._2), // mergeCombiners
+        new HashPartitioner(3)
+      )
+      // .window(Seconds(10))
+      .print()
+
+    ssc.start() // Start the computation
+
+    // Create and push some RDDs into the queue
     val thread = new Thread("pool data source") {
       override def run() {
         while (true) {
-          queue.enqueue(ssc.sparkContext.parallelize(List("to be or not to be , that is the question")))
+          rddQueue.synchronized {
+            rddQueue += ssc.sparkContext.makeRDD(List("to be or not to be , that is the question"))
+          }
           Thread.sleep(1000)
         }
       }
     }
     thread.start()
 
-    val lines = ssc.queueStream(queue)
-
-    // Split each line into words
-    val words = lines.flatMap(_.split(" "))
-
-    // Count each word in each batch
-    val pairs = words.map(word => (word, 1))
-    // val wordCounts = pairs.reduceByKey(_ + _)
-    val wordCounts = pairs.combineByKey(
-      (v) => (v, 1), //createCombiner
-      (acc: (Int, Int), v) => (acc._1 + v, acc._2 + 1), //mergeValue
-      (acc1: (Int, Int), acc2: (Int, Int)) => (acc1._1 + acc2._1, acc1._2 + acc2._2), // mergeCombiners
-      new HashPartitioner(3)
-    )
-
-    // Print the first ten elements of each RDD generated in this DStream to the console
-    wordCounts
-      // .window(Seconds(10))
-      .print()
-
-    ssc.start() // Start the computation
     ssc.awaitTermination() // Wait for the computation to terminate
   }
 }
