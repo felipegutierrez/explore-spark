e2691fb84e66564dc3d1dddbda090df859e21eed
Felipe Gutierrez <felipe.gutierrez@tu-berlin.de>
2020-07-02 17:57:36 +0200

    added warning message where the file '/tmp/datarate.txt' is missing.

--- src/main/scala/org/sense/spark/util/DataRateListener.scala
+++ src/main/scala/org/sense/spark/util/DataRateListener.scala
@@ -7,11 +7,16 @@ import scala.util.control.Exception.allCatch
 
 class DataRateListener extends Thread {
   val DATA_RATE_FILE = "/tmp/datarate.txt"
-  var delayInNanoSeconds: Long = 1000000
+  var delayInNanoSeconds: Long = 1000000000
 
   override def run(): Unit = {
     while (true) {
       val fileName: File = new File(DATA_RATE_FILE)
+
+      if (!fileName.exists()) {
+        println("The file [" + DATA_RATE_FILE + "] does not exist. Hence the DataRate is taking the parameter equal [" + delayInNanoSeconds + "] nanoseconds.")
+        println("Please use \"echo \'1000000000\' > " + DATA_RATE_FILE + "\" to create the data rate file [" + DATA_RATE_FILE + "].")
+      }
 
       val inputBuffer = new BufferedReader(new InputStreamReader(new FileInputStream(fileName), StandardCharsets.UTF_8))
 
