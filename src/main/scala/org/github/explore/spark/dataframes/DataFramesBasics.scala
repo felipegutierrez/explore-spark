e92caaa571d4bd0bbe44d5b38ab4bae721c8dd48
Felipe Gutierrez <felipe.gutierrez@tu-berlin.de>
2021-01-25 18:30:56 +0100

    :construction: answering https://stackoverflow.com/q/65888927/2096986

--- src/main/scala/org/github/explore/spark/dataframes/DataFramesBasics.scala
+++ src/main/scala/org/github/explore/spark/dataframes/DataFramesBasics.scala
@@ -5,9 +5,9 @@ import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.types._
 
 object DataFramesBasics {
-  //  def main(args: Array[String]): Unit = {
-  //    run()
-  //  }
+    def main(args: Array[String]): Unit = {
+      run()
+    }
 
   def run() = {
 
@@ -59,14 +59,7 @@ object DataFramesBasics {
     val cars = Seq(
       ("chevrolet chevelle malibu", 18.0, 8L, 307.0, 130L, 3504L, 12.0, "1970-01-01", "USA"),
       ("buick skylark 320", 15.0, 8L, 350.0, 165L, 3693L, 11.5, "1970-01-01", "USA"),
-      ("plymouth satellite", 18.0, 8L, 318.0, 150L, 3436L, 11.0, "1970-01-01", "USA"),
-      ("amc rebel sst", 16.0, 8L, 304.0, 150L, 3433L, 12.0, "1970-01-01", "USA"),
-      ("ford torino", 17.0, 8L, 302.0, 140L, 3449L, 10.5, "1970-01-01", "USA"),
-      ("ford galaxie 500", 15.0, 8L, 429.0, 198L, 4341L, 10.0, "1970-01-01", "USA"),
-      ("chevrolet impala", 14.0, 8L, 454.0, 220L, 4354L, 9.0, "1970-01-01", "USA"),
-      ("plymouth fury iii", 14.0, 8L, 440.0, 215L, 4312L, 8.5, "1970-01-01", "USA"),
-      ("pontiac catalina", 14.0, 8L, 455.0, 225L, 4425L, 10.0, "1970-01-01", "USA"),
-      ("amc ambassador dpl", 15.0, 8L, 390.0, 190L, 3850L, 8.5, "1970-01-01", "USA")
+      ("plymouth satellite", 18.0, 8L, 318.0, 150L, 3436L, 11.0, "1970-01-01", "USA")
     )
     val manualCarsDF = sparkSession.createDataFrame(cars) // schema auto-inferred
 
@@ -127,5 +120,20 @@ object DataFramesBasics {
     println(s"counting movies: ${moviesDFWithSchema.count()}")
     moviesDFWithSchema.printSchema()
     moviesDFWithSchema.take(10).foreach(println)
+
+    // difference between 2 DFs
+    val carsTwo = Seq(
+      ("chevrolet chevelle malibu", 18.0, 8L, 307.0, 130L, 3504L, 12.0, "1970-01-01", "USA"),
+      ("buick skylark 320", 15.0, 8L, 350.0, 165L, 3693L, 11.5, "1970-01-01", "USA"),
+      ("plymouth satellite", 18.0, 8L, 318.0, 150L, 3436L, 11.0, "1970-01-01", "USA"),
+      ("amc rebel sst", 16.0, 8L, 304.0, 150L, 3433L, 12.0, "1970-01-01", "USA")
+    )
+    val manualCarsTwoDF = sparkSession.createDataFrame(carsTwo)
+    println(s"resultDF union -> distinct:")
+    manualCarsDF.union(manualCarsTwoDF).distinct().show()
+    println(s"resultDF intersect: ")
+    manualCarsDF.intersect(manualCarsTwoDF).show()
+    println(s"resultDF except: ")
+    manualCarsTwoDF.except(manualCarsDF).show()
   }
 }
